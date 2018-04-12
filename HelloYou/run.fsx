d940d264eaf33d8531087a423ba387b7cb8d077d
mikesigs <mikesigsworth@gmail.com>
2018-04-11 22:45:01 -0600

    Added editor prelude

---
+++ HelloYou/run.fsx
@@ -0,0 +1,44 @@
+#if !COMPILED
+
+#I @"C:/Program Files/nodejs/node_modules/azure-functions-core-tools/bin/"
+
+#r "Microsoft.Azure.Webjobs.Host.dll"
+open Microsoft.Azure.WebJobs.Host
+
+#r "System.Net.Http.Formatting.dll"
+#r "System.Web.Http.dll"
+#r "System.Net.Http.dll"
+#r "Newtonsoft.Json.dll"
+
+#else
+
+#r "System.Net.Http"
+#r "Newtonsoft.Json"
+
+#endif
+
+open System.Net
+open System.Net.Http
+open Newtonsoft.Json
+
+type Name = {
+    First: string
+    Last: string
+}
+
+type Greeting = {
+    Greeting: string
+}
+
+let Run(req: HttpRequestMessage, log: TraceWriter) =
+    async {
+        log.Info("Webhook was triggered!")
+        let! jsonContent = req.Content.ReadAsStringAsync() |> Async.AwaitTask
+
+        try
+            let name = JsonConvert.DeserializeObject<Name>(jsonContent)
+            return req.CreateResponse(HttpStatusCode.OK, 
+                { Greeting = sprintf "Hello %s %s!" name.First name.Last })
+        with _ ->
+            return req.CreateResponse(HttpStatusCode.BadRequest)
+    } |> Async.StartAsTask
